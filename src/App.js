e167710278ccfda771000dbd71e1d4b8abda9658
Cordell <cordellbonnieux@gmail.com>
2022-09-20 10:33:22 -0700

    Completed task 2, copied over project and now starting 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { nanoid } from 'nanoid'
+import Todo from './components/Todo'
+import Form from './components/Form'
+import FilterButton from './components/FilterButton'
 
-function App() {
+export default function App(props) {
+  const [ tasks, setTasks ] = useState(props.tasks)
+
+  const toggleTaskCompleted = id => {
+    const updatedTasks = tasks.map( task => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task
+    })
+    setTasks(updatedTasks)
+  }
+
+  const deleteTask =  id => {
+    const remainingTasks = tasks.filter(task => id !== task.id)
+    setTasks(remainingTasks)
+  }
+
+  const addTask = name => {
+    if (name.length > 0) {
+      setTasks([
+        ...tasks,
+        {
+          id: `todo-${nanoid()}`,
+          name,
+          completed: false
+        }
+      ])
+    }
+  }
+
+  const taskList = tasks.map((task) => (
+    <Todo 
+      name={task.name} 
+      completed={task.completed} 
+      id={task.id} 
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ))
+
+  const headingText = `${taskList.length} 
+  ${taskList.length !== 1 ? 'tasks' : 'task'} remaining`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <Form addTask={addTask} />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">
+        {headingText}
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Todo(props) {
+    const { name, completed, id, toggleTaskCompleted, deleteTask } = props
+    return (
+      <li className="todo stack-small">
+        <div className="c-cb">
+          <input 
+            id={id} 
+            type="checkbox" 
+            defaultChecked={completed} 
+            onChange={() => toggleTaskCompleted(id)}
+          />
+          <label className="todo-label" htmlFor={id}>
+            {name}
+          </label>
+        </div>
+        <div className="btn-group">
+          <button type="button" className="btn">
+            Edit <span className="visually-hidden">{name}</span>
+          </button>
+          <button 
+            type="button" 
+            className="btn btn__danger" 
+            onClick={() => deleteTask(id)}
+          >
+            Delete <span className="visually-hidden">{name}</span>
+          </button>
+        </div>
+      </li>
+    );
+}
